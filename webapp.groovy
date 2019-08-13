ebc46d9e23b0f963395c389b3ec60d96e741e7fc
nagaraju07 <53243672+nagaraju07docks@users.noreply.github.com>
2019-08-13 15:56:50 +0530

    Add files via upload
    webapp dsl groovy file added

---
+++ webapp.groovy
@@ -0,0 +1,27 @@
+job('webapp Docker example') {
+    scm {
+        git('git://github.com/nagaraju07docks/maven-webapp.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@gmail.com')
+        }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    steps {
+        maven('verify')
+        maven('clean verify', 'module-a/pom.xml')
+        maven {
+            goals('clean')
+        }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('jcnag/mywebapp')
+            tag('${GIT_REVISION,length=9}')	
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
