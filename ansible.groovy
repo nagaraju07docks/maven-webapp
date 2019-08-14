898495837caa62c339b8bcca9c1b3315e7e4c9d7
nagaraju07 <53243672+nagaraju07docks@users.noreply.github.com>
2019-08-14 16:37:22 +0530

    Update ansible.groovy
    added ansible

--- ansible.groovy
+++ ansible.groovy
@@ -9,7 +9,7 @@ job('webapp Docker example') {
         scm('H/5 * * * *')
     }
     wrappers {
-        sshAgent('99add9e9-84d4-408a-b644-9162a93ee3e4')
+        sshAgent('ansible')
     }
     steps {
     	shell("cd /home/ansible/workspace/ansibleroles; git pull origin master; ansible-playbook -i 192.168.30.50, Docker-Install.yaml; ansible-playbook -i 192.168.30.50, Container-Start.yaml")
