a2c39a2c329dd89cec3e155a38c691d04d796dc7
nagaraju07 <53243672+nagaraju07docks@users.noreply.github.com>
2019-08-14 14:09:18 +0530

    Add files via upload
    ansible groovy added

---
+++ ansible.groovy
@@ -0,0 +1,21 @@
+job('webapp Docker example') {
+    scm {
+    	label('jennode')
+        git('git://github.com/nagaraju07docks/maven-webapp.git', '*/master') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@gmail.com')
+        }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        sshAgent('ansible@192.168.30.40:22')
+    }
+    steps {
+    	shell("cd /home/ansible/workspace/ansibleroles; git pull origin master; ansible-playbook -i 192.168.30.50, Docker-Install.yaml; ansible-playbook -i 192.168.30.50, Container-Start.yaml")
+    }
+
+            }
+        }
+    }
+}
